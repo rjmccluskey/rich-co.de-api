7bd242d46d409c2889329f83029d191b1b571311
rjmccluskey <rjmccluskey@gmail.com>
2015-02-03 19:37:11 -0800

    tests presence of title and content in blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,3 @@
 class Blog < ActiveRecord::Base
+  validates :title, :content, presence: true
 end
--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 RSpec.describe Blog, :type => :model do
   it { should have_db_column(:title) }
   it { should have_db_column(:content) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:content) }
 end
