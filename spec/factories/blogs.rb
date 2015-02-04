b692814e02b7f12a6e58d3c1eacadb89395ebb8a
rjmccluskey <rjmccluskey@gmail.com>
2015-02-03 18:54:31 -0800

    add factory_girl factory and some initial shoulda-matchers for blogs

--- spec/factories/blogs.rb
+++ spec/factories/blogs.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :blog do
-
+    title {Faker::Hacker.say_something_smart}
+    content {Faker::Lorem.paragraph}
   end
 
 end
--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Blog, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_db_column(:title) }
+  it { should have_db_column(:content) }
 end
